795110d13226e61894ae3afde83bc47d906f7c54
kyle-orth <kyle.orth23@gmail.com>
2023-04-20 22:29:23 -0700

    Added FPS calculations in Clock

--- src/GameElements/Clock.java
+++ src/GameElements/Clock.java
@@ -1,15 +1,20 @@
 package GameElements;
 
+import java.util.ArrayList;
+
 public class Clock {
     private long startTime;
     private long tick;
+    private final ArrayList<Long> ticks;
 
     public Clock() {
+        ticks = new ArrayList<>();
         reset();
     }
 
     public void reset() {
         startTime = currentTime();
+        ticks.clear();
         tick();
     }
 
@@ -18,12 +23,18 @@ public class Clock {
     }
 
     public long tick() {
+        // Add the tick time to the storage array ticks
+        ticks.add(currentTime()-tick);
+        if(ticks.size() > 10)
+            ticks.remove(0);
+        // Return the tick time, and set tick to currentTime()
         long sinceLastTick = currentTime() - tick;
         tick = currentTime();
         return sinceLastTick;
     }
 
     public long tick(int maxFrameRate) {
+        // Delay until the given frame time, if necessary, then call tick()
         double nanosPerFrame = 1000000000.0 / maxFrameRate;
         long nanosElapsed = currentTime() - tick;
         if(nanosElapsed < nanosPerFrame)
@@ -37,5 +48,12 @@ public class Clock {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public int frameRate() {
+        long avg = 0;
+        for (long tickTime : ticks)
+            avg += (tickTime / ticks.size());
+        return (int)(1000000000/avg);
     }
 }
