1537bf77fed5419235b9e61f683490fd8aeba677
kyle-orth <kyle.orth23@gmail.com>
2023-04-22 22:07:53 -0700

    Vertical acceleration implemented, need horizontal

--- src/PhysicsEngine/Movement.java
+++ src/PhysicsEngine/Movement.java
@@ -2,11 +2,29 @@ package PhysicsEngine;
 
 import GameElements.Entity;
 
+/**
+ * The Movement class handles simple game physics, excluding collisions. This class handles movement
+ * based on an Entity's velocity, as well as acceleration based on the forces applying to the Entity.
+ */
 public class Movement {
+    private static final double GRAV_CONST = 9.88;
+    private static final double MAX_FREE_FALL = -55.55;
+
     public static void move(Entity e, long milliseconds) {
         double x = e.getX() + e.getvX()*milliseconds/1000.0;
         double y = e.getY() + e.getvY()*milliseconds/1000.0;
         e.setPos(x, y);
     }
 
+    public static void applyGravity(Entity e, long milliseconds) {
+        double acc = GRAV_CONST*milliseconds/1000.0;
+        double vY = Math.max(e.getvY() - acc, MAX_FREE_FALL);
+        e.setVel(e.getvX(), vY);
+    }
+
+    public static void applyGravity(Entity e, long milliseconds, double gravModifier) {
+        double acc = gravModifier*GRAV_CONST*milliseconds/1000.0;
+        double newvY = Math.max(e.getvY() - acc, Math.pow(gravModifier, 2)*MAX_FREE_FALL);
+        e.setVel(e.getvX(), newvY);
+    }
 }
